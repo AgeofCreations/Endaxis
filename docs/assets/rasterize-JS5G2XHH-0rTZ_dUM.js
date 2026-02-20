05e634e820bb401ab020c1d220af5b80a5568b20
AgeofCreations <tesfnatic123@gmail.com>
2026-02-21 02:52:22 +0500

    update build

--- docs/assets/rasterize-JS5G2XHH-0rTZ_dUM.js
+++
@@ -1,1 +0,0 @@
-import{toCanvas as a}from"./toCanvas-DsIXsPbM.js";import"./TimelineEditor-CO3l2tk4.js";import"./index-CCBTej5k.js";import"./effectBindingOptions-BsZoQhNp.js";async function d(r,t){let i=await a(r,t),e=new Image;return e.src=i.toDataURL(`image/${t.format}`,t.quality),await e.decode(),e.style.width=`${i.width/t.dpr}px`,e.style.height=`${i.height/t.dpr}px`,e}export{d as rasterize};
--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,14 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/',
+  base: '/Endaxis/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  test: {}
+  test: {},
+  build: {
+    outDir: 'docs'
+  }
 })
